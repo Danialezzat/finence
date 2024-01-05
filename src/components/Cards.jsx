a36823a506d87e59f1f9ed039888a4347f004309
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-05 05:37:31 -0800

    bug fixed 2

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,7 +9,7 @@ const Cards = () => {
     <div className='w-full py-[10rem] px-4 bg-white '>
         <div className='max-w-[1240px] mx-auto grid md:grid-col-3 gap-8'>
             <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hove:scale-105 duration-500'>
-                <img className='w-20 mx-auto mt-[3rem] bg-white ' src={Single}  />
+                <img className='w-20 mx-auto mt-[3rem] bg-white ' src={Single}  alt='single'/>
                 <h2 className='text-2xl font-bold text-center py-8 '>Single user</h2>
                 <p className='text-center text-4xl font-bold'>$140</p>
                 <div className='text-center font-medium '>
@@ -20,7 +20,7 @@ const Cards = () => {
                 <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
             </div>
             <div className='w-full shadow-xl flex flex-col p-4 my-8 rounded-lg hove:scale-105 duration-500'>
-                <img className='w-20 mx-auto mt-[3rem] bg-white ' src={Double}  />
+                <img className='w-20 mx-auto mt-[3rem] bg-white ' src={Double} alt='double' />
                 <h2 className='text-2xl font-bold text-center py-8 '>Single user</h2>
                 <p className='text-center text-4xl font-bold'>$140</p>
                 <div className='text-center font-medium '>
@@ -31,7 +31,7 @@ const Cards = () => {
                 <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
             </div>
             <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hove:scale-105 duration-500'>
-                <img className='w-20 mx-auto mt-[3rem] bg-white ' src={Three}  />
+                <img className='w-20 mx-auto mt-[3rem] bg-white ' src={Three}  alt='group'/>
                 <h2 className='text-2xl font-bold text-center py-8 '>Single user</h2>
                 <p className='text-center text-4xl font-bold'>$140</p>
                 <div className='text-center font-medium '>
